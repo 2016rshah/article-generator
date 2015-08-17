b44dbd264ef099f602572ba3dd8f75000705120b
Rushi Shah <2016rshah@gmail.com>
2015-08-17 16:39:41 -0400

    Added recaptcha gem and placeholder for api keys

--- Gemfile
+++ Gemfile
@@ -17,6 +17,7 @@ gem 'coffee-rails', '~> 4.0.0'
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 
+gem "recaptcha", :require => "recaptcha/rails"
 
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
 gem 'turbolinks'
---
+++ config/initializers/recaptcha.rb
@@ -0,0 +1,9 @@
+Recaptcha.configure do |config|
+  config.public_key  = '6Lc6BAAAAAAAAChqRbQZcn_yyyyyyyyyyyyyyyyy'
+  config.private_key = '6Lc6BAAAAAAAAKN3DRm6VA_xxxxxxxxxxxxxxxxx'
+  # Uncomment the following line if you are using a proxy server:
+  # config.proxy = 'http://myproxy.com.au:8080'
+  # Uncomment if you want to use the newer version of the API,
+  # only works for versions >= 0.3.7:
+  # config.api_version = 'v2'
+end
